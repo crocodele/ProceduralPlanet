6cdd0e685632eb983a0d6ace9c4be231dcc82432
Viktor Djupsjöbacka <viktor.djupsjobacka@iki.fi>
2023-07-21 01:59:22 +0300

    WIP

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
         index: path.resolve(__dirname, '../dist/ProceduralPlanet/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist/ProceduralPlanet'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/planets/',
+        assetsPublicPath: '/ProceduralPlanet/',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
